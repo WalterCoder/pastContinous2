bc461ac711c01595c85b332f9d4579c450b971cc
Walter Palacios <138240963+WalterCoder@users.noreply.github.com>
2024-02-06 14:29:49 -0600

    Add files via upload

---
+++ englishClass.js
@@ -0,0 +1,9 @@
+function toggle() {
+    var x = document.getElementsByClassName("structure")[0];
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
